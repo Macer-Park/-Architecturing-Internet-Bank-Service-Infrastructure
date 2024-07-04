2dcfd9b59afaaf61c24b22314256a9b43a1e370a
Macer-Park <pkjm1705@gmail.com>
2024-07-04 00:43:22 +0000

    Change server.js for HTTPS

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
 const express = require('express');
 const mysql = require('mysql2');
-// const fs = require('fs'); // 필요 없음
-// const https = require('https'); // 필요 없음
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// HTTPS 옵션 설정 (주석 처리)
-/*
+// HTTPS 옵션 설정
 const httpsOptions = {
-  cert: fs.readFileSync('/etc/letsencrypt/live/www.team4isbest.chungyun.net/fullchain.pem'),
-  key: fs.readFileSync('/etc/letsencrypt/live/www.team4isbest.chungyun.net/privkey.pem')
+  cert: fs.readFileSync('/home/team4/cert/certificate.crt'),
+  key: fs.readFileSync('/home/team4/cert/private.key'),
+  ca: fs.readFileSync('/home/team4/cert/ca_bundle.crt')
 };
 
-// HTTPS 서버 시작
-https.createServer(httpsOptions, app).listen(443, () => {
-  console.log('443 HTTPS 서버 대기중');
-});
-*/
-
-// HTTP에서 HTTPS로 리다이렉션 (주석 처리)
-/*
+// HTTP에서 HTTPS로 리다이렉션
 app.use((req, res, next) => {
   if (!req.secure) {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
   next();
 });
-*/
 
 // index 페이지 라우트
 app.get('/', (req, res) => {
   res.render('index', { user: req.session ? req.session.user : null });
 });
 
-// MySQL 연결 설정 (주석 처리)
 /*
+// MySQL 연결 설정
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'dbuser',
@@ -53,5 +45,10 @@ db.connect((err) => {
 // HTTP 서버 시작
 app.listen(80, () => {
   console.log('80 HTTP 서버 대기중');
+});
+
+// HTTPS 서버 시작
+https.createServer(httpsOptions, app).listen(443, () => {
+  console.log('443 HTTPS 서버 대기중');
 });
 
