5f12a954c2e2d88dec440f00ef6ef8b28b668449
kimtaewook <xodnr2019@naver.com>
2024-07-07 22:24:29 +0900

    주석해제

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,12 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 // HTTPS 옵션 설정 (사용할 경우)
-// const httpsOptions = {
-//   cert: fs.readFileSync('/home/team4/cert/certificate.crt'),
-//   key: fs.readFileSync('/home/team4/cert/private.key'),
-//   ca: fs.readFileSync('/home/team4/cert/ca_bundle.crt')
-// };
+const httpsOptions = {
+  cert: fs.readFileSync('/home/team4/cert/certificate.crt'),
+  key: fs.readFileSync('/home/team4/cert/private.key'),
+  ca: fs.readFileSync('/home/team4/cert/ca_bundle.crt')
+};
+
 // express 미들웨어 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,12 +28,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../frontend/views')); // EJS 템플릿 위치 설정
 
 // HTTP에서 HTTPS로 리다이렉션 (사용할 경우)
-// app.use((req, res, next) => {
-//   if (!req.secure) {
-//     return res.redirect(['https://', req.get('Host'), req.url].join(''));
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+});
 
 // session 미들웨어 설정
 app.use(session({
@@ -56,9 +57,9 @@ app.listen(80, () => {
 });
 
 // // HTTPS 서버 시작 (사용할 경우)
-// https.createServer(httpsOptions, app).listen(443, () => {
-//   console.log('443 HTTPS 서버 대기중');
-// });
+https.createServer(httpsOptions, app).listen(443, () => {
+  console.log('443 HTTPS 서버 대기중');
+});
 
 // 홈 페이지 렌더링
 app.get('/', (req, res) => {
