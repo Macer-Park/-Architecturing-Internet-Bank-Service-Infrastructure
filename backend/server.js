76468195d65e52a279770d39b4587e499feaf269
kimtaewook <xodnr2019@naver.com>
2024-07-08 21:57:50 +0900

    revise csrf

--- backend/server.js
+++ backend/server.js
@@ -19,25 +19,25 @@ const app = express();
 /** https 설정 - 개발시에는 주석처리하고, 푸쉬 할 때는 해제를 해야한다. (반드시)
  * 그래야, 외부서버에서 https 외부서버를 사용할 수 있다.
 */
-//  HTTPS 옵션 설정 (사용할 경우)
-const httpsOptions = {
-  cert: fs.readFileSync('/home/team4/cert/certificate.crt'),
-  key: fs.readFileSync('/home/team4/cert/private.key'),
-  ca: fs.readFileSync('/home/team4/cert/ca_bundle.crt')
-};
+// //  HTTPS 옵션 설정 (사용할 경우)
+// const httpsOptions = {
+//   cert: fs.readFileSync('/home/team4/cert/certificate.crt'),
+//   key: fs.readFileSync('/home/team4/cert/private.key'),
+//   ca: fs.readFileSync('/home/team4/cert/ca_bundle.crt')
+// };
 
-// HTTPS 서버 시작 (사용할 경우)
-https.createServer(httpsOptions, app).listen(443, () => {
-  console.log('443 HTTPS 서버 대기중');
-});
+// // HTTPS 서버 시작 (사용할 경우)
+// https.createServer(httpsOptions, app).listen(443, () => {
+//   console.log('443 HTTPS 서버 대기중');
+// });
 
-// // HTTP에서 HTTPS로 리다이렉션 (사용할 경우)
-app.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
+// // // HTTP에서 HTTPS로 리다이렉션 (사용할 경우)
+// app.use((req, res, next) => {
+//   if (!req.secure) {
+//     return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//   }
+//   next();
+// });
 
 /***********************************************************************************/
 /***********************************************************************************/
@@ -45,9 +45,19 @@ app.use((req, res, next) => {
 // express 미들웨어 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 /****** 미들웨어 설정 *******/
 app.use(express.static(path.join(__dirname, '../frontend/public'))); // frontend 정적 파일 제공
+
+const csrfProtection = csrf({ cookie: true });
+
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 // ejs 미들웨어 설정
 app.set('view engine', 'ejs');
@@ -70,8 +80,8 @@ app.use('/auth', require('./routes/auth'));
 /****** 미들웨어 설정 End *******/
 
 // HTTP 서버 시작
-app.listen(80, () => {
-  console.log('80 HTTP 서버 대기중');
+app.listen(8080, () => {
+  console.log('8080 HTTP 서버 대기중');
 });
 
 // 홈 페이지 렌더링
